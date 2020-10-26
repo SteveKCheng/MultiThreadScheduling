326687d863a16f853580a60e2e6052beea05bddb
Steve Cheng <steve.ckp@gmail.com>
2020-10-26 18:05:33 -0400

    Add project for unit tests

---
+++ test/CpuAffinityTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MultiThreadScheduling.Tests
+{
+    public class CpuAffinityTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
