0e689f5ddcfc1393b6ccb98860d45ca173ec5d1a
Steve Cheng <steve.ckp@gmail.com>
2020-10-27 13:11:51 -0400

    Add unit tests for BitMask

---
+++ test/BitMaskTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace MultiThreadScheduling.Tests
+{
+    public class BitMaskTests
+    {
+        [Fact]
+        public void GetIndexOfNextOnBit()
+        {
+            var mask = new BitMask(stackalloc ulong[4]);
+            mask[2] = true;
+
+            Assert.Equal(2, mask.GetIndexOfNextOnBit(0));
+            Assert.Equal(2, mask.GetIndexOfNextOnBit(1));
+            Assert.Equal(2, mask.GetIndexOfNextOnBit(2));
+            Assert.Equal(-1, mask.GetIndexOfNextOnBit(3));
+            Assert.Equal(-1, mask.GetIndexOfNextOnBit(64));
+        }
+
+        [Fact]
+        public void CountOnBits()
+        {
+            var mask = new BitMask(stackalloc ulong[4]);
+
+            for (int i = 0; i < (4 * 64) / 8; ++i)
+                mask[i * 8 + 3] = true;
+
+            Assert.Equal((4 * 64) / 8, mask.CountOnBits());
+        }
+    }
+}
